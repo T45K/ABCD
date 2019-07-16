994fd1658170d9b9bcf026b68b43eccbeb32ba82
tsk-nkgw <tasktas9@gmail.com>
2019-07-17 00:46:45 +0900

    一旦途中まで

--- src/main/java/com/github/t45k/abcd/ast/BindingASTConstructor.java
+++ src/main/java/com/github/t45k/abcd/ast/BindingASTConstructor.java
@@ -1,13 +1,26 @@
 package com.github.t45k.abcd.ast;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+
 import java.nio.file.Path;
+import java.util.Map;
 
 public class BindingASTConstructor extends AbstractASTConstructor {
     private final Path classpathRootPath;
     private final Path libRootPath;
 
-    public BindingASTConstructor(final Path classpathRootPath, final Path libRootPath) {
+    BindingASTConstructor(final Path classpathRootPath, final Path libRootPath) {
         this.classpathRootPath = classpathRootPath;
         this.libRootPath = libRootPath;
+    }
+
+    @Override
+    public ASTParser createParser() {
+        final ASTParser parser = ASTParser.newParser(AST.JLS10);
+        final Map<String, String> options = getOptionMap();
+        parser.setCompilerOptions(options);
+
+        return parser;
     }
 }
