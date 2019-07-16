ea4e50f8697b5822920d0d6220495df8e7d2ae4a
tsk-nkgw <tasktas9@gmail.com>
2019-07-17 00:45:07 +0900

    impl override method

--- src/main/java/com/github/t45k/abcd/ast/ASTConstructor.java
+++ src/main/java/com/github/t45k/abcd/ast/ASTConstructor.java
@@ -1,4 +1,17 @@
 package com.github.t45k.abcd.ast;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+
+import java.util.Map;
+
 public class ASTConstructor extends AbstractASTConstructor {
+    @Override
+    public ASTParser createParser() {
+        final ASTParser parser = ASTParser.newParser(AST.JLS10);
+        final Map<String, String> options = getOptionMap();
+        parser.setCompilerOptions(options);
+
+        return parser;
+    }
 }
