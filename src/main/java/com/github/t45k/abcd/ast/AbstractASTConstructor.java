04fa6e0cd2f67063ae409c4131bf990a5cc44963
tsk-nkgw <tasktas9@gmail.com>
2019-07-17 00:08:53 +0900

    add create method

--- src/main/java/com/github/t45k/abcd/ast/AbstractASTConstructor.java
+++ src/main/java/com/github/t45k/abcd/ast/AbstractASTConstructor.java
@@ -3,4 +3,11 @@ package com.github.t45k.abcd.ast;
 import java.nio.file.Path;
 
 public class AbstractASTConstructor {
+    public static AbstractASTConstructor create(final Path classpathRootPath, final Path libRootPath) {
+        if (classpathRootPath == null || libRootPath == null) {
+            return new ASTConstructor();
+        }else {
+            return new BindedASTConstructor(classpathRootPath,libRootPath);
+        }
+    }
 }
