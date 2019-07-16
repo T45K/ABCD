2638d432966b836f7de2aa90f7943ba9c131f69e
tsk-nkgw <tasktas9@gmail.com>
2019-07-17 00:10:30 +0900

    add constructor

--- src/main/java/com/github/t45k/abcd/ast/BindedASTConstructor.java
+++ src/main/java/com/github/t45k/abcd/ast/BindedASTConstructor.java
@@ -1,4 +1,13 @@
 package com.github.t45k.abcd.ast;
 
+import java.nio.file.Path;
+
 public class BindedASTConstructor extends AbstractASTConstructor {
+    private final Path classpathRootPath;
+    private final Path libRootPath;
+
+    public BindedASTConstructor(final Path classpathRootPath, final Path libRootPath) {
+        this.classpathRootPath = classpathRootPath;
+        this.libRootPath = libRootPath;
+    }
 }
