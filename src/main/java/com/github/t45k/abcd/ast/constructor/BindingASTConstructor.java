116d75516a56d12b45d92422cba2cc20b7f45e76
tsk-nkgw <tasktas9@gmail.com>
2019-07-18 14:43:01 +0900

    impl

--- src/main/java/com/github/t45k/abcd/ast/constructor/BindingASTConstructor.java
+++ src/main/java/com/github/t45k/abcd/ast/constructor/BindingASTConstructor.java
@@ -3,9 +3,12 @@ package com.github.t45k.abcd.ast.constructor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BindingASTConstructor extends AbstractASTConstructor {
     private final Path targetFileRootPath;
@@ -24,11 +27,32 @@ public class BindingASTConstructor extends AbstractASTConstructor {
         final Map<String, String> options = getOptionMap();
         parser.setCompilerOptions(options);
 
+        setParseEnvironment(parser);
+
+        parser.setResolveBindings(true);
+        parser.setBindingsRecovery(true);
+
+        return parser;
+    }
+
+    private void setParseEnvironment(final ASTParser parser) {
         final List<Path> classpathList = getTargetFiles(this.libRootPath, ".jar");
         classpathList.add(this.classpathRootPath);
         final String[] classpathEntries = convertPathListToStringArray(classpathList);
-        parser.setEnvironment(classpathEntries, null, null, false);
 
-        return parser;
+        final List<Path> sourcePathList = getTargetDirs(this.targetFileRootPath);
+        final String[] sourcePathEntries = convertPathListToStringArray(sourcePathList);
+
+        parser.setEnvironment(classpathEntries, sourcePathEntries, null, false);
+    }
+
+    private List<Path> getTargetDirs(final Path rootPath) {
+        try {
+            return Files.walk(rootPath)
+                    .filter(Files::isDirectory)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("invalid argument was specified");
+        }
     }
 }
