817a847cf7998b02e5a37c5e27054826c82398b3
tsk-nkgw <tasktas9@gmail.com>
2019-07-20 20:15:46 +0900

    add constructor

--- src/main/java/com/github/t45k/abcd/clone/entity/CloneSet.java
+++ src/main/java/com/github/t45k/abcd/clone/entity/CloneSet.java
@@ -1,12 +1,11 @@
 package com.github.t45k.abcd.clone.entity;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class CloneSet {
-    private final Set<CodeFragment> cloneSet = new HashSet<>();
+    private final Set<CodeFragment> cloneSet;
 
-    public void add(final CodeFragment codeFragment) {
-        this.cloneSet.add(codeFragment);
+    public CloneSet(final Set<CodeFragment> cloneSet) {
+        this.cloneSet = cloneSet;
     }
 }
