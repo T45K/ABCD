96a55a8cabf9d67e3d878b72145167e14ccafe2c
tsk-nkgw <tasktas9@gmail.com>
2019-07-21 02:14:09 +0900

    add sample

---
+++ sample/cloneDetectionSample/src/detection/A.java
@@ -0,0 +1,15 @@
+package detection;
+
+public class A {
+    public method() {
+        if (true) {
+            int a = 0;
+            int b = 1;
+        }
+
+        if (true) {
+            int a = 0;
+            int b = 1;
+        }
+    }
+}
