ca285a0c8feecf3bdee936105e4b2584454e8ea9
tsk-nkgw <tasktas9@gmail.com>
2019-07-20 20:52:59 +0900

    add interface

--- src/main/java/com/github/t45k/abcd/clone/detection/visitor/CodeFragmentNormalizer.java
+++
@@ -1,7 +0,0 @@
-package com.github.t45k.abcd.clone.detection.visitor;
-
-import org.eclipse.jdt.core.dom.Statement;
-
-public interface CodeFragmentNormalizer {
-    String normalize(final Statement codeFragment);
-}
