e08096ffb3fedfb77eba2234e6dfbfe0ff8be348
tsk-nkgw <tasktas9@gmail.com>
2019-07-20 20:16:12 +0900

    add method

--- src/main/java/com/github/t45k/abcd/clone/detection/visitor/CodeFragmentFindingVisitor.java
+++ src/main/java/com/github/t45k/abcd/clone/detection/visitor/CodeFragmentFindingVisitor.java
@@ -1,6 +1,35 @@
 package com.github.t45k.abcd.clone.detection.visitor;
 
+import com.github.t45k.abcd.ast.FileAST;
+import com.github.t45k.abcd.clone.detection.DetectionMode;
+import com.github.t45k.abcd.clone.entity.CodeFragment;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class CodeFragmentFindingVisitor extends ASTVisitor {
+    private final DetectionMode mode;
+    private final Path filePath;
+    private final Set<CodeFragment> codeFragments;
+
+    public CodeFragmentFindingVisitor(final DetectionMode mode, final Path filePath) {
+        this.mode = mode;
+        this.filePath = filePath;
+        this.codeFragments = new HashSet<>();
+    }
+
+    public static Stream<CodeFragment> findCodeFragments(final DetectionMode mode, final FileAST fileAST) {
+        final Path filePath = fileAST.getPath();
+        final CodeFragmentFindingVisitor visitor = new CodeFragmentFindingVisitor(mode, filePath);
+        fileAST.getUnit().accept(visitor);
+
+        return visitor.codeFragments.stream();
+    }
+
+    public Set<CodeFragment> getCloneSetMap() {
+        return this.codeFragments;
+    }
 }
