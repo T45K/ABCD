42dae5ae439cfedf6720b5ba2bfa4e033f4ed8d3
T45K <tasktas9@gmail.com>
2019-07-21 22:49:26 +0900

    add tests

---
+++ src/test/java/com/github/t45k/abcd/clone/detection/normalizer/Type2NormalizerTest.java
@@ -0,0 +1,30 @@
+package com.github.t45k.abcd.clone.detection.normalizer;
+
+import com.github.t45k.abcd.ast.FileAST;
+import com.github.t45k.abcd.ast.constructor.ASTConstructor;
+import org.eclipse.jdt.core.compiler.InvalidInputException;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Type2NormalizerTest {
+    @Test
+    public void testNormalization() throws InvalidInputException {
+        final CodeFragmentNormalizer normalizer = new Type2Normalizer();
+        final String normalizedFragment = normalizer.normalize(getCompilationUnit());
+        assertThat(normalizedFragment).isEqualTo("package $ ; public class $ { private static void $ ( ) { int $ = $ ; assert $ ; } }");
+    }
+
+    private CompilationUnit getCompilationUnit() {
+        final Path path = Paths.get("sample/cloneDetectionSample/src/normalizer");
+        final ASTConstructor astConstructor = ASTConstructor.create(path, null, null);
+        final Set<FileAST> fileASTS = astConstructor.constructFileAST(path);
+        final FileAST fileAST = fileASTS.iterator().next();
+        return fileAST.getUnit();
+    }
+}
