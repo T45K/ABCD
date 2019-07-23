331cd47d4e7da8ddb5a6d1db905b75cb17830b6c
tsk-nkgw <tasktas9@gmail.com>
2019-07-23 20:39:07 +0900

    add inherit class

---
+++ src/main/java/com/github/t45k/abcd/output/XMLFormat.java
@@ -0,0 +1,10 @@
+package com.github.t45k.abcd.output;
+
+import com.github.t45k.abcd.clone.entity.CloneSet;
+
+public class XMLFormat implements OutputFormat {
+    @Override
+    public void output(final CloneSet cloneSet) {
+
+    }
+}
