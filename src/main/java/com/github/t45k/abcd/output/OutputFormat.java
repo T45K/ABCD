13d32867bebc83180f3898d746ea404707e332b8
tsk-nkgw <tasktas9@gmail.com>
2019-07-23 20:41:25 +0900

    add create method

--- src/main/java/com/github/t45k/abcd/output/OutputFormat.java
+++ src/main/java/com/github/t45k/abcd/output/OutputFormat.java
@@ -4,4 +4,19 @@ import com.github.t45k.abcd.clone.entity.CloneSet;
 
 public interface OutputFormat {
     void output(CloneSet cloneSet);
+
+    public static OutputFormat create(final Format format) {
+        switch (format) {
+            case JSON:
+                return new JsonFormat();
+            case CSV:
+                return new CSVFormat();
+            case TXT:
+                return new TextFormat();
+            case XML:
+                return new XMLFormat();
+        }
+
+        throw new RuntimeException("You could not come here");
+    }
 }
