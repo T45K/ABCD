fa8b08881c0a8af2fb83821fab9950a305783410
tsk-nkgw <tasktas9@gmail.com>
2019-07-25 01:22:16 +0900

    rename and add method

---
+++ src/main/java/com/github/t45k/abcd/output/Output.java
@@ -0,0 +1,27 @@
+package com.github.t45k.abcd.output;
+
+import com.github.t45k.abcd.clone.entity.CloneSet;
+
+import java.nio.file.Path;
+import java.util.Set;
+
+public interface Output {
+    void output(final Path filePath, final Set<CloneSet> cloneSets);
+
+    String convertCloneSetToString(final CloneSet cloneSet);
+
+    public static Output create(final Format format) {
+        switch (format) {
+            case JSON:
+                return new OutputJson();
+            case CSV:
+                return new OutputCSV();
+            case TXT:
+                return new OutputText();
+            case XML:
+                return new OutputXML();
+        }
+
+        throw new RuntimeException("You could not come here");
+    }
+}
