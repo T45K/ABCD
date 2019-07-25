c79ae1346ee4c293100ad0c0903b2b42329d3774
tsk-nkgw <tasktas9@gmail.com>
2019-07-25 15:03:49 +0900

    subtle fix

--- src/main/java/com/github/t45k/abcd/output/AbstractOutput.java
+++ src/main/java/com/github/t45k/abcd/output/AbstractOutput.java
@@ -15,7 +15,7 @@ public abstract class AbstractOutput implements Output {
     public void output(final Path filePath, final Set<CloneSet> cloneSets) throws IOException {
         final AtomicInteger index = new AtomicInteger(1);
         final String fileContents = cloneSets.stream()
-                .map(cloneSet -> index.getAndIncrement() + "\n" + convertCloneSetToString(cloneSet))
+                .map(cloneSet -> "clone set " + index.getAndIncrement() + "\n" + convertCloneSetToString(cloneSet))
                 .collect(Collectors.joining("\n\n"));
 
         final Path outputFilePath = filePath.resolve(getExtension());
