531af4966af9a3fc28bb042e4fc1d174a1275720
T45K <tasktas9@gmail.com>
2019-07-28 16:08:51 +0900

    add test

---
+++ src/test/java/com/github/t45k/abcd/ConfigTest.java
@@ -0,0 +1,36 @@
+package com.github.t45k.abcd;
+
+import com.github.t45k.abcd.clone.detection.DetectionMode;
+import com.github.t45k.abcd.output.Format;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ConfigTest {
+
+    @Test
+    public void testConfig() {
+        final String[] args = {"-s", ".", "-d", "3", "-o", "./output", "-f", "txt", "-b", ".", "-l", ".", "-tl", "5", "-tt", "20"};
+        final Config config = Config.Builder.buildFromCmdLineArgs(args);
+
+        assertThat(config.getSrcDir()).isEqualByComparingTo(Paths.get("."));
+        assertThat(config.getDetectionMode()).isEqualByComparingTo(DetectionMode.ORIGINAL);
+        assertThat(config.getOutputFile()).isEqualByComparingTo(Paths.get("./output"));
+        assertThat(config.getFormat()).isEqualByComparingTo(Format.TXT);
+        assertThat(config.getBinaryDir()).isEqualByComparingTo(Paths.get("."));
+        assertThat(config.getLibDir()).isEqualByComparingTo(Paths.get("."));
+        assertThat(config.getThresholdLine()).isEqualByComparingTo(5);
+        assertThat(config.getThresholdToken()).isEqualByComparingTo(20);
+    }
+
+    @Test
+    public void testError() {
+        final String[] args = {"-p"};
+        assertThatThrownBy(() -> Config.Builder.buildFromCmdLineArgs(args))
+                .isInstanceOfSatisfying(RuntimeException.class, e -> {
+                });
+    }
+}
