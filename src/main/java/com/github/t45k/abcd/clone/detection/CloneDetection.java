262a4e48f7767af5c40a9f234cfb183c0a94d272
T45K <tasktas9@gmail.com>
2019-07-28 21:27:15 +0900

    fix

--- src/main/java/com/github/t45k/abcd/clone/detection/CloneDetection.java
+++ src/main/java/com/github/t45k/abcd/clone/detection/CloneDetection.java
@@ -1,5 +1,6 @@
 package com.github.t45k.abcd.clone.detection;
 
+import com.github.t45k.abcd.Config;
 import com.github.t45k.abcd.ast.FileAST;
 import com.github.t45k.abcd.clone.detection.visitor.CodeFragmentFindingVisitor;
 import com.github.t45k.abcd.clone.entity.CloneSet;
@@ -10,14 +11,16 @@ import java.util.stream.Collectors;
 
 public class CloneDetection {
     private final DetectionMode mode;
+    private final Config config;
 
-    public CloneDetection(final DetectionMode mode) {
+    public CloneDetection(final DetectionMode mode, final Config config) {
         this.mode = mode;
+        this.config = config;
     }
 
     public Set<CloneSet> detectClones(final Set<FileAST> fileASTs) {
         return fileASTs.stream()
-                .flatMap(fileAST -> CodeFragmentFindingVisitor.findCodeFragments(this.mode, fileAST))
+                .flatMap(fileAST -> CodeFragmentFindingVisitor.findCodeFragments(this.mode, fileAST, config))
                 .collect(Collectors.groupingBy(
                         CodeFragment::getHashValue,
                         Collectors.toSet()
