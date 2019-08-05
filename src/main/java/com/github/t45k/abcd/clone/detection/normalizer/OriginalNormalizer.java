e4fff25efc516fc56c4f355d1938d76f02e6bc93
T45K <tasktas9@gmail.com>
2019-08-05 15:25:37 +0900

    inherit

--- src/main/java/com/github/t45k/abcd/clone/detection/normalizer/OriginalNormalizer.java
+++ src/main/java/com/github/t45k/abcd/clone/detection/normalizer/OriginalNormalizer.java
@@ -1,8 +1,9 @@
 package com.github.t45k.abcd.clone.detection.normalizer;
 
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 
-public class OriginalNormalizer implements CodeFragmentNormalizer {
+public class OriginalNormalizer extends ASTVisitor implements CodeFragmentNormalizer {
     @Override
     public String normalize(final ASTNode codeFragment) {
         return null;
